cb0dc53fa78f61d8af5648ccea4f6ff55257fac9
JoSilvaInfo <joanafsilva.info@gmail.com>
2023-03-30 17:32:49 +0100

    Moving paltform and pop-up trap

---
+++ Assets/Scripts/EnvironmentalSpikes.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnvironmentalSpikes : MonoBehaviour
+{
+    [SerializeField]  private float MoveSpeed = 3f;
+    [SerializeField] private float waitTimeUp;
+    [SerializeField] private float waitTimeDown;
+    [SerializeField] private bool cycle;
+
+    public GameObject waypointsUp;
+    public GameObject waypointsDown;
+
+    private void Awake()
+    {
+        StartCoroutine(MovePauseAndReturn());
+    }
+    private void Update()
+    {
+        if(cycle)
+        {
+            cycle = false;
+            StartCoroutine(MovePauseAndReturn());
+        }
+    }
+
+    IEnumerator MovePauseAndReturn()
+    {
+        yield return new WaitForSeconds(waitTimeDown);
+        yield return MoveTo(waypointsDown.transform);
+        yield return new WaitForSeconds(waitTimeUp);
+        yield return MoveTo(waypointsUp.transform);
+        cycle = true;
+    }
+    IEnumerator MoveTo(Transform destination)
+    {
+        while (transform.position != destination.position)
+        {
+            transform.position = Vector2.MoveTowards(transform.position, destination.position, MoveSpeed * Time.deltaTime);
+            yield return null;
+        }
+    }
+}
