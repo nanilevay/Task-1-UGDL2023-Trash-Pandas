f432697ff7a934fbf158b253302170c62f761494
Diana Levay <dianaalevay@gmail.com>
2023-03-31 02:24:39 +0100

    Fixes on prefabs

--- Assets/Scripts/Goal.cs
+++ Assets/Scripts/Goal.cs
@@ -1,11 +1,13 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Goal : MonoBehaviour
 {
     private void OnTriggerEnter2D(Collider2D collider)
     {
         GameEventsManager.instance.GoalReached();
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
     }
 }
